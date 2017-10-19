94cda822806109052ff4ece767d2449a218aabd1
Benjamin Cooper <bencooper222@gmail.com>
2017-10-19 13:42:04 -0500

    Finished project
    Not very resilient

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const config = require('./config');
 const twilio = require('twilio');
 
+// require your json files here to make life easier
 
-function sendText(to,body, mediaURL = "") {
+const users = require('./vh.json');
+//const shittyCompanies = require('./sample_send.json); // Comcast and AT&T numbers
+
+
+function sendText(to, body, mediaURL = "") {
     let client = new twilio(config.twilio.uid, config.twilio.auth);
 
 
@@ -13,32 +18,32 @@ function sendText(to,body, mediaURL = "") {
         from: config.twilio.phone // From a valid Twilio number
     };
 
-    if(mediaURL===""){
-        delete twilioParams['mediaUrl'];
+    if (mediaURL === "") {
+        delete twilioParams['mediaUrl']; // if there's no URL (pls don't pass invalid URLs) #resilientcode
     }
     client.messages.create(twilioParams).then((message) => console.log(message.sid));
 }
 
 
 
-function sendToGroup(json, key="", body, mediaUrl = ""){
-    let receipients = [];
-    if(key===""){
-        receipients = json;
-    }
-    else{
-        try{
-            recipients = json[key];
-        }
-        catch(e){
+function sendToGroup(json, body, key = "", mediaUrl = "") {
+    let recipients = [];
+    if (key === "") {
+        recipients = json; // if it's just a js array
+    } else {
+        try {
+            recipients = json[key]; // find the proper key
+        } catch (e) {
             console.log("Invalid key");
             return;
         }
     }
 
-    for(let i=0; i< recipients.length; i++){
-        sendText(receipients[i],body, mediaUrl)
+    for (let i = 0; i < recipients.length; i++) {
+        sendText(recipients[i], body, mediaUrl)
     }
 }
 
-sendText('2246594934',"Heyyy");
+//sendText('2246594934',"Heyyy");
+
+sendToGroup(users, "yo", "nerds");
---
+++ sample_config.js
@@ -0,0 +1,9 @@
+let config = {};
+config.twilio = {};
+
+config.twilio.uid = "TWILIO UID";
+config.twilio.auth = "TWILIO AUTH TOKEN";
+config.twilio.phone = "TWILIO PHONE NUMBER TO TEXT FROM"
+
+
+module.exports = config;    
